1b567ffd29758f7e3892978b8ed0c13a571b0280
Niava <candicevivien@gmail.com>
2020-03-14 23:12:27 +1100

    fix herok

---
+++ stan/dist/build/precache-manifest.6c01f68ac489886cd455085b58f5c8d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f30aa840eaddd29cbab05d8df6d2fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfff115a3081c03ba2fe",
+    "url": "./static/css/main.335be1e2.chunk.css"
+  },
+  {
+    "revision": "8735479691e7f6c74941",
+    "url": "./static/js/2.28a302c7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.28a302c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfff115a3081c03ba2fe",
+    "url": "./static/js/main.7391eec6.chunk.js"
+  },
+  {
+    "revision": "de9bbb412aa6cc19a371",
+    "url": "./static/js/runtime-main.75c7075f.js"
+  },
+  {
+    "revision": "0e8d88c032de674865def29a169c2ba7",
+    "url": "./static/media/app-store.0e8d88c0.svg"
+  },
+  {
+    "revision": "80cf88b9776a2ae8f06e70667374393b",
+    "url": "./static/media/facebook-white.80cf88b9.svg"
+  },
+  {
+    "revision": "efd131f69a0b5f91f7d4f08dfd4c8620",
+    "url": "./static/media/instagram-white.efd131f6.svg"
+  },
+  {
+    "revision": "e017762f3e7c0198d6a0e54a2c8a0ed9",
+    "url": "./static/media/play-store.e017762f.svg"
+  },
+  {
+    "revision": "2dd9b9385ac5f45a8656441ad14d19de",
+    "url": "./static/media/twitter-white.2dd9b938.svg"
+  },
+  {
+    "revision": "f0450282133e83188f1ec9a53092713f",
+    "url": "./static/media/windows-store.f0450282.svg"
+  }
+]);
