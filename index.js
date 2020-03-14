180672a6ae9c6806e7d7d7f86d2c030597bedb3c
Niava <candicevivien@gmail.com>
2020-03-14 22:50:16 +1100

    Fix -heroku

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //import express for use
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'stan/dist')));
-
 //express will serve index.html file if not recognize the route
 //if dont know the path, just kick the user back to index.html
-const path = require('path');
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'stan/dist/index.html'));
 });
